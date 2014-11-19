db28a86f7d0f447fcc4e73360bd952e1d8584675
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2014-11-20 00:42:15 +0100

    cursor movement is now relative to previous position

--- zad4.py
+++ zad4.py
@@ -1,4 +1,5 @@
 import cv2
+import cv2.cv as cv
 import numpy as np
 from pymouse import PyMouse
 
@@ -15,15 +16,11 @@ while( cap.isOpened() and closeFlag != 1):
     if not ret:
         continue
 
-
     gray = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur( gray, (5,5), 0 )
-    blur2 = cv2.GaussianBlur( gray, (5,5), 0 )
-    blur3 = cv2.GaussianBlur( gray, (5,5), 0 )
-    blur4 = cv2.GaussianBlur( gray, (5,5), 0 )
 
-    ret,thresh = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV)
-    ret,thresh2 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV)
+    ret,thresh = cv2.threshold(blur,80,255,cv2.THRESH_BINARY_INV)
+    ret,thresh2 = cv2.threshold(blur,80,255,cv2.THRESH_BINARY_INV)
     cv2.imshow('treshold', thresh2)
 
     contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
@@ -49,9 +46,14 @@ while( cap.isOpened() and closeFlag != 1):
                 cx = int(moments['m10']/moments['m00']) # cx = M10/M00
                 cy = int(moments['m01']/moments['m00']) # cy = M01/M00
 
+    previous_centr = centr if 'centr' in globals() else (cx,cy)
     centr=(cx,cy)
+    px, py = previous_centr
 
-    m.move(cx*2, cy*2)
+    dx = px - cx
+    dy = cy - py
+    cursor_x, cursor_y = m.position()
+    m.move(cursor_x + dx, cursor_y + dy)
 
     cv2.circle(img,centr,10,[0,0,255],2)
 
@@ -63,8 +65,6 @@ while( cap.isOpened() and closeFlag != 1):
 
 
     cv2.imshow('input', img)
-    #cv2.imshow('threshold2', threshold2)
-    #cv2.imshow('contours', drawing)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
